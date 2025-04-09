94385ca008d85d1fa966ed8b70e8c221af3dfc11
CesarPradoJr <cesarhenrique_prado@hotmail.com>
2025-04-09 15:19:16 -0300

    Finish project Form

--- src/app/componente11/componente11.component.ts
+++ src/app/componente11/componente11.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Pessoa } from '../modelo/Pessoa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-componente11',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './componente11.component.html',
   styleUrl: './componente11.component.css'
 })
@@ -18,4 +20,69 @@ export class Componente11Component {
 
   //Visibilidade dos botoes
   btnCadastrar:boolean = true;
+
+  //Vetor
+  vetor:Pessoa[] = [];
+
+  //Armazenar indice da pessoa selecionada
+  indice:number = -1
+
+  //Funcao de cadastro
+  cadastrar(){
+    //Cadastro no vetor
+    this.vetor.push(this.formulario.value as Pessoa);
+
+    //Limpeza do inputs
+    this.formulario.reset();
+
+    //Visualização via console
+    console.table(this.vetor);
+  }
+
+  //Funcao de Selecao
+  selecionar(indice:number){
+    //Atribuir o indice da pessoa
+    this.indice = indice;
+
+    //Atribuir os dados da pessoa no formulário
+    this.formulario.setValue({
+      nome: this.vetor[indice].nome,
+      idade: this.vetor[indice].idade,
+      cidade: this.vetor[indice].cidade
+    });
+
+    //Visibilidade dos botoes
+    this.btnCadastrar = false;
+  }
+
+  //Funcao de Alteracao
+  alterar(){
+    //Alterar vetor
+    this.vetor[this.indice] = this.formulario.value as Pessoa;
+
+    //Limpar os inputs
+    this.formulario.reset();
+
+    //Visibilidade dos botoes
+    this.btnCadastrar = true;
+
+  }
+
+  //Funcao de remocao
+  remover(){
+
+    this.vetor.splice(this.indice, 1);
+
+    this.formulario.reset();
+
+    this.btnCadastrar = true;
+  }
+
+  //Funca de cancelar
+  cancelar(){
+     
+    this.formulario.reset();
+
+    this.btnCadastrar = true;
+  }
 }
